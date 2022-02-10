a274dd4267b22b6fb196fa0c9b975a86b1bb9e24
bridgetp24 <bridgetpeterkin@gmail.com>
2022-02-10 12:42:28 -0600

    Initial Commit

---
+++ SimpleAudioPlayer/SimpleAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAudioPlayerApp.swift
+//  SimpleAudioPlayer
+//
+//  Created by Bridget Peterkin on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
